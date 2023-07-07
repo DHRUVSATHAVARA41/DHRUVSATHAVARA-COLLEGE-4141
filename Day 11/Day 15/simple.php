c6ef480b8ddd517b7227a0d208152821974ab48e
DHRUVSATHAVARA41 <137283658+DHRUVSATHAVARA41@users.noreply.github.com>
2023-07-07 10:47:06 -0400

    Add files via upload

---
+++ Day 11/Day 15/simple.php
@@ -0,0 +1,16 @@
+<?php
+$silveroak="hello";
+$a="silver oak university";
+$b="how are you";
+echo "$a <br> $b <br>";
+echo "hello world";
+
+
+$x=10;
+$y=20;
+$sum=$x+$y;
+$sub=$x-$y;
+$mul=$x*$y;
+$div=$x/$y;
+echo " <br> $sum <br> $sub <br> $mul <br> $div <br> ";
+?>
---
+++ Day 11/Day 17/arraysort.php
@@ -0,0 +1,10 @@
+<?php
+echo "The orignal array is:<br>";
+$a=array("darshan","meet","zeel","param","hidad");
+echo "<pre>";
+print_r($a);
+echo "After sorting...<br>";
+sort($a);
+echo"<pre>";
+print_r($a);
+?>
